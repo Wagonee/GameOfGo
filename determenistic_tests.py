ddb3038d823223e3da8fe8e3db656f3c5df401f8
Wagonee <ponomarev.ny@yandex.ru>
2025-03-09 20:21:30 +0300

    queues developing

--- determenistic_tests.py
+++ determenistic_tests.py
@@ -7,6 +7,7 @@ from dlgo.utils import print_board, print_move, clear_screen
 
 import time
 
+from dlgo.game_turn import DeterministicQueue
 
 def main():
     board_size = 9
@@ -15,15 +16,25 @@ def main():
         gotypes.Player.black: agent.random_bot.RandomBot(),
         gotypes.Player.white: agent.random_bot.RandomBot(),
     }
+
+    queue = DeterministicQueue("BWBBWW")
+
     while not game.is_over:
         time.sleep(0.3)
 
         clear_screen()
         print(scoring.compute_game_result(game), '\n')
         print_board(game.board)
-        bot_move = bots[game.next_player].select_move(game)
-        print_move(game.next_player, bot_move)
-        game = game.apply_move(bot_move)
+
+        # Получаем следующий ход из очереди
+        next_turn = queue.get_next_turn()
+        if next_turn is None:
+            bot_move = bots[game.next_player].select_move(game)
+            queue.add_turn(bot_move)
+            next_turn = queue.get_next_turn()
+
+        print_move(next_turn.player, next_turn.move)
+        game = game.apply_move(next_turn.move)
 
 
 if __name__ == '__main__':
